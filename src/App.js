6926518f294c07a240b40dac3bf3730205d60e15
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-08-05 11:26:36 +0530

    Router added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
+import { Switch, Route, Redirect } from "react-router-dom";
+
 import Heading from './Components/heading';
-
-
-
-
+import First from './Components/first';
 import "nes.css/css/nes.min.css";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Heading />
+      
+
+      <Switch>
+
+        <Route exact path="/" component = {First} />
+        <Route exact path="/head" component = {Heading} />
+        <Redirect to="/"></Redirect>
+
+      </Switch>
     </div>
   );
 }
