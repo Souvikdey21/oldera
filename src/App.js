8b7aeab90217db3eeca51e73a09b5d4f46c7ec41
Shubham Jha <shubham.jha2001m@gmail.com>
2020-08-15 02:14:33 +0700

    Setting Up The Components

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import './App.css';
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Heading from './Components/heading';
-import First from './Components/first';
+import First, { Name } from './Components/first';
 import NavBar from "./Components/nav";
+import Menu from './Components/MenuComponent';
+import Calculator from './Components/Calculator';
+import Notepad from './Components/Notepad';
+import ToDo from './Components/ToDo';
 
 
 import "nes.css/css/nes.min.css";
@@ -18,14 +22,16 @@ import "nes.css/css/nes.min.css";
 function App() {
   return (
     <div className="App">
-      
-    <NavBar />
 
     <br />
       <Switch>
 
         <Route exact path="/" component = {First} />
         <Route exact path="/head" component = {Heading} />
+        <Route exact path="/menu" component={() => <Menu name={Name} />} />
+        <Route exact path="/calculator" component = {() => <Calculator />} />
+        <Route exact path="/notepad" component = {() => <Notepad />} />
+        <Route exact path="/todo" component={() => <ToDo />} />
         <Redirect to="/"></Redirect>
 
       </Switch>
---
+++ src/Components/MenuComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+import Page from './Page';
+
+function Menu({name}) {
+    return(
+        <div>
+            <Page />
+        </div>
+    );
+}
+
+export default Menu;
